ed41a985af3bf498ec99a2ec6c4ac13067adcbe2
jesus martinez <jesus.alberto.vk@gmail.com>
2020-10-06 00:33:10 -0500

    Fix: integrate command cobra kai

--- src/lib/channels/index.ts
+++ src/lib/channels/index.ts
@@ -4,7 +4,8 @@ const channels = {
   rulesChannel: '724806034769575988',
   organization: '738997872594518017',
   moderation: '735244606215553125',
-  welcome: '726134324746911786'
+  welcome: '726134324746911786',
+  general: '724813649071308831'
 }
 
 export default channels
--- src/lib/commands/stupidCommands.ts
+++ src/lib/commands/stupidCommands.ts
@@ -19,6 +19,14 @@ const stupidCommands: Command[] = [
     exec(dispatcher: StupidDispatcher, message: Message) {
       dispatcher.love(message)
     },
+  },
+  {
+    id: "!ck",
+    description: "Les enseña que una victoria sin honor ni dignidinad es una lección muy cara a pagar",
+    type: CommandType.STUPID,
+    exec(dispatcher: StupidDispatcher, message: Message) {
+      dispatcher.cobraKai(message)
+    },
   }
 ]
 
