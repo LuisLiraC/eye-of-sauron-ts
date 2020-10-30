4cc6940e7b10db9c93e4e4c8a32a0eebcf443566
jesus martinez <jesus.alberto.vk@gmail.com>
2020-10-29 22:27:31 -0600

    Features: create command marcode, gaming call, atencion call and add regex (instagram|fb)

--- src/dispatchers/StupidDispatcher.ts
+++ src/dispatchers/StupidDispatcher.ts
@@ -17,6 +17,46 @@ class StupidDispatcher extends Dispatcher {
     }
   }
 
+  getToWorkMarcode(message: Message) {
+    try {
+      const channel = getChannelById(message, this.channels.undefinedDevsBots)
+      const marcoId = this.uds.find(ud => ud.name === 'Marcode')?.id
+      
+      channel instanceof TextChannel
+        && channel.send(`Ya ponte a programar de verdad <@${marcoId}> , https://www.youtube.com/watch?v=VTLAX5IZY0A`)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  sendAtentionCall(message: Message) {
+    try {
+      const user = message.mentions.users.first()
+      if (user) {
+        const member = message.guild?.member(user)
+        if (member) {
+          const channel = getChannelById(message, this.channels.general)
+
+          channel instanceof TextChannel
+            && channel.send(`<@${member}> Hemos eliminado tu publicación por favor lee las reglas...`)
+        }
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  sendGameCall(message: Message) {
+    try {
+      const channel = getChannelById(message, this.channels.gaming)
+      channel instanceof TextChannel && channel.send(`Vamos a jugar @everyone `)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   cobraKai(message: Message) {
     try {
       const channel = getChannelById(message, this.channels.general)
--- src/lib/commands/stupidCommands.ts
+++ src/lib/commands/stupidCommands.ts
@@ -21,6 +21,30 @@ const stupidCommands: Command[] = [
     },
   },
   {
+    id: "!mp",
+    description: "Le dice a marco que empiece a programar de verdad",
+    type: CommandType.STUPID,
+    exec(dispatcher: StupidDispatcher, message: Message) {
+      dispatcher.getToWorkMarcode(message)
+    },
+  },
+  {
+    id: "!ca",
+    description: "Que deje de publicar cosas que consideramos fuera de lugar ( ca @user )",
+    type: CommandType.STUPID,
+    exec(dispatcher: StupidDispatcher, message: Message) {
+      dispatcher.sendAtentionCall(message)
+    },
+  },
+  {
+    id: "!cg",
+    description: "A jugar todo el mundo",
+    type: CommandType.STUPID,
+    exec(dispatcher: StupidDispatcher, message: Message) {
+      dispatcher.sendGameCall(message)
+    },
+  },
+  {
     id: "!ck",
     description: "Les enseña que una victoria sin honor ni dignidinad es una lección muy cara a pagar",
     type: CommandType.STUPID,
